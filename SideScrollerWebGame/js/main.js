faaba1bfea1b33713c9e66ecd77efb16c61cfad2
Gil Emerson <emerson-gil@outlook.com>
2014-11-03 20:58:10 -0500

    Added in main Style Sheet & main JavaScript file & an Images folder

---
+++ SideScrollerWebGame/js/main.js
@@ -0,0 +1,9 @@
+﻿/*
+File Name: Side Scroller Web Game
+Last Modified By: Gil Emerson
+Date Last Modified: 
+    Program Description: Create an original 2D side scrolling arcade game (left to right & right to left).
+The game must have a start screen, (that includes game instructions for the users)
+                         the main game screen, and a end game screen. 
+    Revision History: #2
+    */
